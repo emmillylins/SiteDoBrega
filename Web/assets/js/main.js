f0d43695cfdcb3d4f0c2ad703ad56152414e0a2b
emmy <104332292+emmillylins@users.noreply.github.com>
2024-06-24 14:58:45 -0300

    Add files via upload

--- Web/assets/js/main.js
+++ Web/assets/js/main.js
@@ -66,3 +66,103 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
   }
 });
+  
+function addEventOnClick() {
+    const links = document.querySelectorAll('a[data-id]');
+    links.forEach(link => {
+        link.addEventListener('click', async function(event) {
+            event.preventDefault(); // Previne a navegação padrão
+
+            const categoryId = this.getAttribute('data-id');
+            const categoryUrl = this.getAttribute('href');
+
+            // Chama a função getfaixas com o ID da categoria
+            const faixas = await getFaixas(categoryId);
+
+            // Redireciona para a URL após a função ser chamada
+            window.location.href = categoryUrl;
+        });
+    });
+}
+
+function popularNavbar(categorias) {
+  const navbarUl = document.querySelector('#dynamic-navbar');
+  categorias.forEach(categoria => {
+    if (categoria.ativo) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+
+      a.href = categoria.url;
+      a.textContent = categoria.desc;
+      a.setAttribute('data-id', categoria.id);
+
+      // Adicionar evento de clique
+      a.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede o redirecionamento imediato
+        localStorage.setItem('categoriaId', categoria.id);
+        window.location.href = categoria.url; // Redireciona após salvar o id
+      });
+
+      li.appendChild(a);
+      navbarUl.appendChild(li);
+    }
+  });
+}
+
+// Função para popular o grid de categorias
+function popularGrid(categorias) {
+  const categoriesContainer = document.getElementById('categories-container');
+
+  if (categorias.length === 0) {
+    categoriesContainer.innerHTML = '<p style="color: rgba(255, 255, 255, 0.6); font-size: 14px;">nenhuma categoria disponível</p>';
+    return;
+  }
+
+  let count = 0; // Contador para controlar a alternância das classes  
+  categorias.forEach(category => {
+    let colClass;
+    
+    // Definir a classe com base no valor do contador
+    if (count % 3 === 0) {
+      colClass = 'col-md-5';
+    } else {
+      colClass = 'col-md-7';
+    }  
+    // Incrementar o contador, resetando a cada 3
+    count = (count + 1) % 3;
+
+    const categoryDiv = document.createElement('div');
+    categoryDiv.id = 'cat';
+    categoryDiv.className = colClass;
+
+    const link = document.createElement('a');
+    link.href = category.url;
+    link.dataset.id = category.id;
+    link.innerHTML = `
+      <img src="${category.img}" alt="${category.desc}">
+      <div class="desc">${category.desc}</div>
+    `;
+
+    // Adicionar evento de clique
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Impede o redirecionamento imediato
+      localStorage.setItem('categoriaId', category.id);
+      window.location.href = category.url; // Redireciona após salvar o id
+    });
+
+    categoryDiv.appendChild(link);
+    categoriesContainer.appendChild(categoryDiv);
+  });
+}
+
+function carregarFaixas() {
+  const categoriaId = localStorage.getItem('categoriaId');
+  if (categoriaId) {
+      getFaixas(categoriaId).then(faixas => {
+          console.log(faixas);
+          // Aqui você pode adicionar código para exibir as faixas na página
+      });
+  } else {
+      console.error('Categoria ID não encontrado');
+  }
+}
--- Web/assets/js/service.js
+++ Web/assets/js/service.js
@@ -1,84 +1,49 @@
 
+async function getFaixas(categoriaId) {
+  try {
+    // Simulação de uma requisição assíncrona
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const url = `https://localhost:5000/api/faixas/por-categoria/${categoriaId}`;
+
+    const response = await fetch(url);
+    const result = await response.json();
+    
+    if (result.success) {
+      result.data.forEach(faixa => {
+        faixa.link = faixa.link.replace(/'/g, `"`);
+      });
+      return result.data;
+    } 
+    else {
+      console.error('Erro ao buscar Faixas:', result.message);
+      return [];
+    }
+
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error);
+    return [];
+  }
+}
+
 async function getCategorias() {
     try {
       const response = await fetch('https://localhost:5000/api/categorias');
       const result = await response.json();
-      if (result.success) {
-        // Itera sobre os dados para ajustar o caminho da imagem
-      result.data.forEach(category => {
-        // Ajuste da URL da imagem para usar o caminho relativo
-        category.img = `${category.img}`;
-      });
 
+      if (result.success) {
+        result.data.forEach(categoria => {
+          if (categoria.url != "#") categoria.url = categoria.url + '.html';
+        });
         return result.data;
-      } else {
+      } 
+      else {
         console.error('Erro ao buscar categorias:', result.message);
         return [];
       }
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Erro ao fazer a requisição:', error);
       return [];
     }
 }
-
-function construirNavbar(categorias) {
-  const navbarUl = document.querySelector('#navbar .dropdown ul');
-  categorias.forEach(categoria => {
-      if (categoria.ativo) {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.href = categoria.url + ".html";
-      a.textContent = categoria.desc;
-      li.appendChild(a);
-      navbarUl.appendChild(li);
-      }
-  });
-}
-
-// Função para popular o grid de categorias
-function popularGrid(categorias) {
-  const categoriesContainer = document.getElementById('categories-container');
-
-  if (categorias.length === 0) {
-    categoriesContainer.innerHTML = '<p>Nenhuma categoria encontrada.</p>';
-    return;
-  }
-
-  let count = 0; // Contador para controlar a alternância das classes
-
-  categorias.forEach(category => {
-    let colClass;
-    
-    // Definir a classe com base no valor do contador
-    if (count % 3 === 0) {
-      colClass = 'col-md-5';
-    } else {
-      colClass = 'col-md-7';
-    }
-
-    // Incrementar o contador, resetando a cada 3
-    count = (count + 1) % 3;
-
-    const categoryDiv = document.createElement('div');
-    categoryDiv.id = 'cat';
-    categoryDiv.className = colClass;
-
-    categoryDiv.innerHTML = `
-      <a href="${category.url}.html">
-        <img src="${category.img}" alt="${category.desc}">
-        <div class="desc">${category.desc}</div>
-      </a>
-    `;
-    categoriesContainer.appendChild(categoryDiv);
-  });
-}
-
-// Função de inicialização
-async function init() {
-    const categorias = await getCategorias();
-    construirNavbar(categorias);
-    popularGrid(categorias);
-}
-
-// Chamando a função init ao carregar a página
-document.addEventListener('DOMContentLoaded', init);

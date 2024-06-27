eb11b4f93b1847b3fe64c8a64aafe307be90a836
emmy <104332292+emmillylins@users.noreply.github.com>
2024-06-27 10:39:03 -0300

    Update FaixaValidator.cs

--- BackEnd/Domain/Validators/FaixaValidator.cs
+++ BackEnd/Domain/Validators/FaixaValidator.cs
@@ -12,6 +12,7 @@ namespace Domain.Validators
             RuleFor(x => x.Artista).MaximumLength(100).WithMessage("O campo '{PropertyName}' deve ter até {MaxLength} caracteres.");
 
             RuleFor(x => x.Link).NotNull().NotEmpty().WithMessage("O campo '{PropertyName}' é obrigatório.")
+                .MinimumLength(200).WithMessage("O campo '{PropertyName}' não está no formato correto.")
                 .MaximumLength(500).WithMessage("O campo '{PropertyName}' deve ter até {MaxLength} caracteres.");
 
             RuleFor(x => x.NomeUsuario).NotNull().NotEmpty().WithMessage("O campo '{PropertyName}' é obrigatório.");
